5d9be00bf0cca50dd2dd490faf1ae21d9ad61c18
anthonyvallejo23 <anthony.vallejo23@my.stjohns.edu>
2026-02-12 16:51:14 -0500

    Update Vehicle.java
    Moved vehicle.java to Vehicle.java in frontend

--- frontend/classes/Vehicle.java
+++ frontend/classes/Vehicle.java
@@ -1,4 +1,42 @@
 package classes;
-public class Vehicle {
+//Focus on the backend and make necessary classes to store information for the user and their vehicles (Sebastian)
+
+// vehicle class
+//The system shall receive and verify the vehicle's number, license plate, make, and model.
+
+public class vehicle { 
+    private int number;
+    private String make;
+    private String model;
+    private String licensePlate;
+
+
+    public vehicle(int number, String make, String model , String licensePlate) {
+        this.number = number;
+        this.make = make;
+        this.model = model;
+        this.licensePlate = licensePlate;
+
+        //make sure vehicle has all the necessary information
+    if(number == 0 || make == null || model == null || licensePlate == null){
+        throw new IllegalArgumentException("Vehicle information is incomplete");
+    }
+    }
+
+    //implementing the getters in order to access the private variables
+
+    public int getNumber() {
+        return number;
+    }
+    public String getMake() {
+        return make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+    
 
 }
