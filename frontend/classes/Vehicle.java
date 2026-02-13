24897c1abe38ec72135699591203fc78eb73e9cb
Ivan <ivanlin32475@gmail.com>
2026-02-12 19:50:09 -0500

    Username and password will now be saved to a .txt file and made username case insensitive so no dupe username will be created

--- frontend/classes/Vehicle.java
+++ frontend/classes/Vehicle.java
@@ -5,14 +5,14 @@ package classes;
 // vehicle class
 //The system shall receive and verify the vehicle's number, license plate, make, and model.
 
-public class vehicle { 
+public class Vehicle { 
     private int number;
     private String make;
     private String model;
     private String licensePlate;
 
 
-    public vehicle(int number, String make, String model , String licensePlate) {
+    public Vehicle(int number, String make, String model , String licensePlate) {
         this.number = number;
         this.make = make;
         this.model = model;
