4e1570097b7205f9616c4344beed7d15ae0e586c
Anthony <anthony.vallejo23@my.stjohns.edu>
2026-02-15 21:59:00 -0500

    Fixed comment in Client.java

--- frontend/classes/Client.java
+++ frontend/classes/Client.java
@@ -12,7 +12,7 @@ public class Client extends User { // utilizes the User class to set client user
 
     private String clientId;
 
-    //constructor for the client class; utilizes the constructor of the User class to set the username and password, and also sets the clientId, approximateJobDuration, and jobDeadline.
+    //constructor for the client class; utilizes the constructor of the User class to set the username and password, and also sets the clientId.
     public Client(String username, String password, String clientId) {
         super(username, password, "");//email is not needed for clients, so we can set it to an empty string
         this.clientId = clientId;
